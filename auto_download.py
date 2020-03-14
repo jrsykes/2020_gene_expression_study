cf9e711b02a61902fe40b89e67b5be227b1884f3
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-03-14 11:38:21 +0000

    .

--- auto_download.py
+++ auto_download.py
@@ -7,25 +7,6 @@ species = input("species_name: ")
 
 dat = pd.read_csv("/home/sykesj/dat/SRA_list_refined.csv", header=None)
 
-for index, row in dat.iterrows():
-	try:
-		if row[0] == species:
-			species = row[0]
-			SRR = row[1]
-			sex = row[2]
-			layout = row[3]
-
-			command = 'sbatch /home/sykesj/scripts/2020_gene_expression_study/new_download.sh ' + species + ' ' + SRR + ' ' + sex + ' ' + layout
-			#subprocess.Popen([command], shell=True)
-			time.sleep(20)
-			check = str(subprocess.check_output('squeue', shell=True))
-			
-			while 'sykesj' in check:
-				time.sleep(60)
-				check = str(subprocess.check_output('squeue', shell=True))
-	except:
-		pass
-
 df_paired = pd.DataFrame()
 df_single = pd.DataFrame()
 
@@ -39,10 +20,29 @@ for index, row in dat.iterrows():
 print (df_paired)
 print (df_single)
 
-
-
-
 exit()
+
+
+for index, row in dat.iterrows():
+	try:
+		if row[0] == species:
+			species = row[0]
+			SRR = row[1]
+			sex = row[2]
+			layout = row[3]
+
+			command = 'sbatch /home/sykesj/scripts/2020_gene_expression_study/new_download.sh ' + species + ' ' + SRR + ' ' + sex + ' ' + layout
+			#subprocess.Popen([command], shell=True)
+			#time.sleep(20)
+			check = str(subprocess.check_output('squeue', shell=True))
+			
+			while 'sykesj' in check:
+				time.sleep(60)
+				check = str(subprocess.check_output('squeue', shell=True))
+	except:
+		pass
+
+
 
 command = 'sbatch /home/sykesj/scripts/2020_gene_expression_study/trinity_busco_blast.sh ' + species + ' ' + layout
 subprocess.Popen([command], shell=True)

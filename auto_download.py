5472984a9ceae6e98a06cd2097fc4313a267ef98
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-03-14 11:29:45 +0000

    .

--- auto_download.py
+++ auto_download.py
@@ -26,7 +26,22 @@ for index, row in dat.iterrows():
 	except:
 		pass
 
-print (layout)
+df_paired = pd.DataFrame()
+df_single = pd.DataFrame()
+
+for index, row in dat.iterrows():
+	try:
+		if row[0] == species:
+			if row[3] == PAIRED:
+				df_paired.append(row)
+			if row[3] == SINGLE:
+				df_single.append(row)
+
+print (df_paired)
+print (df_single)
+
+
+
 
 exit()
 

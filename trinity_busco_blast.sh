813136af244d90caabea3403b45a9d9a1751254e
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-03-20 15:56:55 +0000

    .

--- trinity_busco_blast.sh
+++ trinity_busco_blast.sh
@@ -29,24 +29,26 @@ trinity_busco_blast () {
 		echo $RIGHT >> /projects/sykesj/analyses/$SPECIES/trinity/path.txt
 
 	
-		/home/sykesj/software/trinityrnaseq-v2.9.1/Trinity --SS_lib_type RF --seqType fq --left $LEFT --right $RIGHT --CPU 20 --max_memory 100G --output \
-			/scratch/projects/sykesj/paired_trinity_$SPECIES && rsync -a /scratch/projects/sykesj/paired_trinity_$SPECIES/Trinity.fasta /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly.fa
+		#/home/sykesj/software/trinityrnaseq-v2.9.1/Trinity --SS_lib_type RF --seqType fq --left $LEFT --right $RIGHT --CPU 20 --max_memory 100G --output \
+			#/scratch/projects/sykesj/paired_trinity_$SPECIES && rsync -a /scratch/projects/sykesj/paired_trinity_$SPECIES/Trinity.fasta /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly.fa
 
 ###### filter out < 1000 bp ##########
 
-		python3 /home/sykesj/scripts/2020_gene_expression_study/1k_filter.py /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly.fa \
-			/projects/sykesj/analyses/$SPECIES/trinity/paired_assembly_1k.fa && rm -f /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly.fa
+		#python3 /home/sykesj/scripts/2020_gene_expression_study/1k_filter.py /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly.fa \
+			#/projects/sykesj/analyses/$SPECIES/trinity/paired_assembly_1k.fa && rm -f /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly.fa
 
 ###### busco ##########
 
-		python /home/sykesj/software/busco-master/src/busco/run_BUSCO.py -f --config /home/sykesj/software/busco-master/config/config.ini -i /projects/sykesj/analyses/$SPECIES/trinity/paired_assembly_1k.fa -o busco_paired_$SPECIES \
-		-l arthropoda_odb10 -m tran -c 16 \
-		&& mkdir /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries \
-		&& mv /projects/sykesj/analyses/temp_out/trinity/run_busco_paired_$SPECIES /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries \
-		&& python /home/sykesj/software/busco-master/scripts/generate_plot.py -wd /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries/run_busco_paired_$SPECIES \
-		&& mv /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries/run_busco_paired_$SPECIES/busco_figure.png /projects/sykesj/analyses/$SPECIES/busco/busco_paired_$SPECIES.png \
-		&& rm -rf /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries \
-		&& mv /home/sykesj/software/busco*.log /projects/sykesj/analyses/$SPECIES/busco/
+		python /home/sykesj/software/busco-master/src/busco/run_BUSCO.py -f --config /home/sykesj/software/busco-master/config/config.ini -i \
+			/projects/sykesj/analyses/$SPECIES/trinity/paired_assembly_1k.fa -o busco_paired_$SPECIES \
+			-l arthropoda_odb10 -m tran -c 16 \
+			&& mv /scratch/projects/sykesj/BUSCO_tmp/busco_paired_$SPECIES/short_summary.specific.arthropoda_odb10.busco_paired_$SPECIES.txt /projects/sykesj/analyses/$SPECIES/busco/BUSCO_out_$SPECIES.txt
+			#&& mkdir /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries \
+			#&& mv /projects/sykesj/analyses/temp_out/trinity/run_busco_paired_$SPECIES /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries \
+			#&& python /home/sykesj/software/busco-master/scripts/generate_plot.py -wd /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries/run_busco_paired_$SPECIES \
+			#&& mv /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries/run_busco_paired_$SPECIES/busco_figure.png /projects/sykesj/analyses/$SPECIES/busco/busco_paired_$SPECIES.png \
+			#&& rm -rf /projects/sykesj/analyses/$SPECIES/busco/busco_paired_summaries \
+			#&& mv /home/sykesj/software/busco*.log /projects/sykesj/analyses/$SPECIES/busco/
 
 ########## blast ##########
 
@@ -73,25 +75,27 @@ trinity_busco_blast () {
 		echo $INPUT > /projects/sykesj/analyses/$SPECIES/trinity/path.txt
 
 	
-		/home/sykesj/software/trinityrnaseq-v2.9.1/Trinity --seqType fq --single $INPUT --CPU 32 --max_memory 100G --output /scratch/projects/sykesj/single_trinity_$SPECIES \
-			&& rsync -a /scratch/projects/sykesj/single_trinity_$SPECIES/Trinity.fasta /projects/sykesj/analyses/$SPECIES/trinity/single_assembly.fa
+		#/home/sykesj/software/trinityrnaseq-v2.9.1/Trinity --seqType fq --single $INPUT --CPU 32 --max_memory 100G --output /scratch/projects/sykesj/single_trinity_$SPECIES \
+			#&& rsync -a /scratch/projects/sykesj/single_trinity_$SPECIES/Trinity.fasta /projects/sykesj/analyses/$SPECIES/trinity/single_assembly.fa
 
 
 ###### filter < 1000 bp ##########
 
-		python3 /home/sykesj/scripts/2020_gene_expression_study/1k_filter.py /projects/sykesj/analyses/$SPECIES/trinity/single_assembly.fa \
-			/projects/sykesj/analyses/$SPECIES/trinity/single_assembly_1k.fa && rm -f /projects/sykesj/analyses/$SPECIES/trinity/single_assembly.fa
+		#python3 /home/sykesj/scripts/2020_gene_expression_study/1k_filter.py /projects/sykesj/analyses/$SPECIES/trinity/single_assembly.fa \
+			#/projects/sykesj/analyses/$SPECIES/trinity/single_assembly_1k.fa && rm -f /projects/sykesj/analyses/$SPECIES/trinity/single_assembly.fa
 
 ###### busco ##########
 
-		python /home/sykesj/software/busco-master/src/busco/run_BUSCO.py -f --config /home/sykesj/software/busco-master/config/config.ini -i /projects/sykesj/analyses/$SPECIES/trinity/single_assembly_1k.fa -o busco_single_$SPECIES \
+		python /home/sykesj/software/busco-master/src/busco/run_BUSCO.py -f --config /home/sykesj/software/busco-master/config/config.ini -i \
+			/projects/sykesj/analyses/$SPECIES/trinity/single_assembly_1k.fa -o busco_single_$SPECIES \
 			-l arthropoda_odb10 -m tran -c 16 \
-			&& mkdir /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries \
-			&& mv /projects/sykesj/analyses/temp_out/trinity/run_busco_single_$SPECIES /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries \
-			&& python /home/sykesj/software/busco-master/scripts/generate_plot.py -wd /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries/run_busco_single_$SPECIES \
-			&& mv /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries/run_busco_single_$SPECIES/busco_figure.png /projects/sykesj/analyses/$SPECIES/busco/busco_single_$SPECIES.png \
-			&& rm -rf /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries \
-			&& mv /home/sykesj/software/busco*.log /projects/sykesj/analyses/$SPECIES/busco/
+			&& mv /scratch/projects/sykesj/BUSCO_tmp/busco_single_$SPECIES/short_summary.specific.arthropoda_odb10.busco_single_$SPECIES.txt /projects/sykesj/analyses/$SPECIES/busco/BUSCO_out_$SPECIES.txt
+#			&& mkdir /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries \
+#			&& mv /projects/sykesj/analyses/temp_out/trinity/run_busco_single_$SPECIES /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries \
+#			&& python /home/sykesj/software/busco-master/scripts/generate_plot.py -wd /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries/run_busco_single_$SPECIES \
+#			&& mv /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries/run_busco_single_$SPECIES/busco_figure.png /projects/sykesj/analyses/$SPECIES/busco/busco_single_$SPECIES.png \
+#			&& rm -rf /projects/sykesj/analyses/$SPECIES/busco/busco_single_summaries \
+#			&& mv /home/sykesj/software/busco*.log /projects/sykesj/analyses/$SPECIES/busco/
 
 
 ########## blast ##########
@@ -117,8 +121,8 @@ trinity_busco_blast () {
 }
 
 
-multiqc /projects/sykesj/analyses/$SPECIES/fastqc/ -o /projects/sykesj/analyses/$SPECIES/fastqc/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc/SRR*
-multiqc /projects/sykesj/analyses/$SPECIES/fastqc2/ -o /projects/sykesj/analyses/$SPECIES/fastqc2/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc2/SRR*
+#multiqc /projects/sykesj/analyses/$SPECIES/fastqc/ -o /projects/sykesj/analyses/$SPECIES/fastqc/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc/SRR*
+#multiqc /projects/sykesj/analyses/$SPECIES/fastqc2/ -o /projects/sykesj/analyses/$SPECIES/fastqc2/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc2/SRR*
 
 
 trinity_busco_blast $SPECIES $LAYOUT

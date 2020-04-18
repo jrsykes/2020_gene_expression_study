a1c26ce77d960b9fb88cccd7eefc0ae15d65ffbb
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-04-18 18:16:40 +0100

    .

--- trinity_busco_blast_old.sh
+++ trinity_busco_blast_old.sh
@@ -123,26 +123,28 @@ trinity_busco_blast () {
 rm -rf /scratch/projects/sykesj/BUSCO_tmp/busco_"$LAYOUT"_"$SPECIES"
 
 
-multi_qc () {
+####################
+# fastqc & multiqc
 
-	if [ $LAYOUT == 'PAIRED' ]
-	then
-		TRIMMED_LIBS=$(for file in $(ls /projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/male/*.fq /projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/female/*.fq) ; do readlink -f $file; done | paste -sd " " - )
-	
-	elif [ $LAYOUT == 'SINGLE' ]
-	then
-		TRIMMED_LIBS=$(for file in $(ls /projects/sykesj/analyses/$SPECIES/trimmomatic/SINGLE/male/*.fq /projects/sykesj/analyses/$SPECIES/trimmomatic/SINGLE/female/*.fq \
-			/projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/male/*.fq /projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/female/*.fq) ; do readlink -f $file; done | paste -sd " " - )
-	fi
-	
-	/home/sykesj/software/FastQC/fastqc --outdir /projects/sykesj/analyses/$SPECIES/fastqc2 $TRIMMED_LIBS \
-		&& multiqc /projects/sykesj/analyses/$SPECIES/fastqc/ -o /projects/sykesj/analyses/$SPECIES/fastqc/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc/SRR* \
-		&& multiqc /projects/sykesj/analyses/$SPECIES/fastqc2/ -o /projects/sykesj/analyses/$SPECIES/fastqc2/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc2/SRR*
-	
-}
+if [ $LAYOUT == 'PAIRED' ]
+then
+	TRIMMED_LIBS=$(for file in $(ls /projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/male/*.fq /projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/female/*.fq) ; do readlink -f $file; done | paste -sd " " - )
+elif [ $LAYOUT == 'SINGLE' ]
+then
+	TRIMMED_LIBS=$(for file in $(ls /projects/sykesj/analyses/$SPECIES/trimmomatic/SINGLE/male/*.fq /projects/sykesj/analyses/$SPECIES/trimmomatic/SINGLE/female/*.fq \
+		/projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/male/*.fq /projects/sykesj/analyses/$SPECIES/trimmomatic/PAIRED/female/*.fq) ; do readlink -f $file; done | paste -sd " " - )
+fi
 
 
-multiqc $SPECIES $LAYOUT
+
+/home/sykesj/software/FastQC/fastqc --outdir /projects/sykesj/analyses/$SPECIES/fastqc2 $TRIMMED_LIBS \
+	&& multiqc /projects/sykesj/analyses/$SPECIES/fastqc/ -o /projects/sykesj/analyses/$SPECIES/fastqc/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc/SRR* \
+	&& multiqc /projects/sykesj/analyses/$SPECIES/fastqc2/ -o /projects/sykesj/analyses/$SPECIES/fastqc2/ && rm -f /projects/sykesj/analyses/$SPECIES/fastqc2/SRR*
+
+#####################
+
+
+
 trinity_busco_blast $SPECIES $LAYOUT
 
 

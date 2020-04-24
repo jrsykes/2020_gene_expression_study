d45db2360c5368b96c9d1c266941322b8a6dbc0b
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-04-24 18:39:14 +0100

    .

--- cali.py
+++ cali.py
@@ -2,7 +2,7 @@
 #SBATCH --partition=long
 #SBATCH --time=5-00:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=100gb
+#SBATCH --mem=200gb
 #SBATCH --ntasks=20
 #SBATCH --output=/home/sykesj/scripts/StdOut/R-%x.%j.out
 #SBATCH --error=/home/sykesj/scripts/StdOut/R-%x.%j.err
@@ -48,15 +48,33 @@ FinalOutfile_dir = str(sys.argv[2]) + '/out/'
 
 print('\nBuilding list of blast IDs \n')
 
-blast_id_list = []
+out = CaliWDout + 'blast_id_list.csv'
+final_df = pd.DataFrame()
 
-for item in blast_list:
-	df = pd.read_csv(item, sep='\t', usecols = [4], dtype = str)
-	for index, row in df.iterrows():
-		if row[0] not in blast_id_list:
-			blast_id_list.append(row[0])
+def blast_id_list_builder():
+	blast_id_list = []
+	for item in blast_list:
+		df = pd.read_csv(item, sep='\t', usecols = [4], dtype = str)
+		for index, row in df.iterrows():
+			if row[0] not in blast_id_list:
+				blast_id_list.append(row[0])
+	blast_id_list = list(dict.fromkeys(blast_id_list))
+	#blast_id_list.insert(0, '.')
+	#blast_id_list.insert(0, '.')
+	#blast_id_list.insert(0, '.')
+	final_df['Blast_ID'] = blast_id_list
+	final_df.to_csv(out, index=False)
 
-blast_id_list = list(dict.fromkeys(blast_id_list))
+
+command = 'ls ' + CaliWDout
+check = str(subprocess.check_output(command, shell=True))
+
+if 'blast_id_list.csv' not in check:
+	blast_id_list_builder()
+	blast_id_list = pd.read_csv(out, header=None, dtype = str)
+else:
+	blast_id_list = pd.read_csv(out, header=None, dtype = str)
+
 
 
 print('Blast ID list built \n')
@@ -68,7 +86,7 @@ print('Blast ID list built \n')
 ###############################
 
 
-print('Compiling Trinity IDs, Blast IDs & TMP counts \n')
+
 
 
 ####################################################
@@ -107,19 +125,9 @@ def compiler(chunk):
 						pass
 
 
-chunk_size = int(dat.shape[0]/20)
-chunks = [dat.iloc[dat.index[i:i + chunk_size]] for i in range(0, dat.shape[0], chunk_size)]
-
-pool = mp.Pool(processes=20)
+   
 
 
-result = pool.map(compiler, chunks)
-
-pool.close()    
-
-
-
-print('Complilation complete \n')
 
 
 
@@ -128,41 +136,46 @@ print('Complilation complete \n')
 ###########################################################
 # Create data frame with all contigs related to blast IDs and tpm. Then write to CSV
 
+
+def ID_tpm_combiner(chunk):
+	for index, row in dat.iterrows():
+		species = row[0]
+		SRR = row[1]
+		sex = row[2]
+		SexDeterm = row[4]
+		tpm_list = []
+		tpm_list.append(species)
+		tpm_list.append(SexDeterm)
+		tpm_list.append(sex)
+		cali_abundance_file = FinalOutfile_dir + SRR + '_CaliOut.csv'
+		cali_abundance_df = pd.read_csv(cali_abundance_file, header=None, dtype={0: str, 1: str, 2: float})
+		for i in blast_id_list.values.tolist()[1:]:
+			search_out = cali_abundance_df[cali_abundance_df[1].str.match(str(i))]
+			tpm = search_out[2].sum()
+			tpm_list.append(tpm)
+		final_df[SRR] = tpm_list
+	
+
+n_processes = 20
+chunk_size = int(dat.shape[0]/n_processes)
+chunks = [dat.iloc[dat.index[i:i + chunk_size]] for i in range(0, dat.shape[0], chunk_size)]
+
+print('Compiling Trinity IDs, Blast IDs & TMP counts \n')
+
+pool = mp.Pool(processes=n_processes)
+result = pool.map(compiler, chunks)
+pool.close() 
+
+print('Complilation complete \n')
+
 print('Producing final output file \n')
 
-
-final_df = pd.DataFrame()
-
-blast_id_list.insert(0, '.')
-blast_id_list.insert(0, '.')
-blast_id_list.insert(0, '.')
+pool = mp.Pool(processes=n_processes)
+result = pool.map(ID_tpm_combiner, chunks)
+pool.close()  
 
 
-final_df['Blast_ID'] = blast_id_list
 
-for index, row in dat.iterrows():
-	species = row[0]
-	SRR = row[1]
-	sex = row[2]
-	SexDeterm = row[4]
-	
-	tpm_list = []
-	tpm_list.append(species)
-	tpm_list.append(SexDeterm)
-	tpm_list.append(sex)
-
-	cali_abundance_file = FinalOutfile_dir + SRR + '_CaliOut.csv'
-	cali_abundance_df = pd.read_csv(cali_abundance_file, header=None, dtype={0: str, 1: str, 2: float})
-
-	for i in blast_id_list:
-		search_out = cali_abundance_df[cali_abundance_df[1].str.match(i)]
-		tpm = search_out[2].sum()
-
-
-		tpm_list.append(tpm)
-
-	final_df[SRR] = tpm_list
-	
 out = FinalOutfile_dir + 'CaliOut.csv'
 final_df.to_csv(out, index=False)
 
--- cali.sh
+++ cali.sh
@@ -7,6 +7,6 @@
 #SBATCH --output=/home/sykesj/scripts/StdOut/R-%x.%j.out
 #SBATCH --error=/home/sykesj/scripts/StdOut/R-%x.%j.err
 
-python3 /home/sykesj/scripts/2020_gene_expression_study/cali.py /home/sykesj/dat/SRA_list_refined.csv /scratch/projects/sykesj/CaliWD
+sbatch /home/sykesj/scripts/2020_gene_expression_study/cali.py /home/sykesj/dat/SRA_list_refined.csv /scratch/projects/sykesj/CaliWD
 
 python3 /home/jamie/Documents/2020_gene_expression_study/scripts/cali.py /home/jamie/Documents/2020_gene_expression_study/cali_test/SRA_list_refined.csv /home/jamie/Documents/2020_gene_expression_study/cali_test/CaliWD

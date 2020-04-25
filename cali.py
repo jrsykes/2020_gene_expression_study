15bf84b893ac30fe8c154d17473ecf582e25d4b7
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-04-25 18:45:30 +0100

    .

--- cali.py
+++ cali.py
@@ -3,11 +3,11 @@
 #SBATCH --time=5-00:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=200gb
-#SBATCH --ntasks=15
+#SBATCH --ntasks=20
 #SBATCH --output=/home/sykesj/scripts/StdOut/R-%x.%j.out
 #SBATCH --error=/home/sykesj/scripts/StdOut/R-%x.%j.err
 
-n_processes = 12
+n_processes = 20
 
 
 import pandas as pd

6a5c8758a4aa24b76d59a7ba8af237347d95f289
Jamie Sykes <jamie_r_sykes@outlook.com>
2020-04-25 18:44:34 +0100

    .

--- cali.py
+++ cali.py
@@ -7,7 +7,7 @@
 #SBATCH --output=/home/sykesj/scripts/StdOut/R-%x.%j.out
 #SBATCH --error=/home/sykesj/scripts/StdOut/R-%x.%j.err
 
-n_processes = 15
+n_processes = 12
 
 
 import pandas as pd
@@ -15,6 +15,7 @@ import subprocess
 import sys
 import os
 import multiprocessing as mp
+from random import random
 ####################################
 # Parsing input files
 
@@ -38,8 +39,6 @@ for i in os.listdir(CaliWDin):
 	elif 'abundance.filtered.tsv' in i:
 		abundace_list.append(CaliWDin + i)
 
-FinalOutfile_dir = str(sys.argv[2]) + '/out/'
-
 
 ##################################
 
@@ -49,8 +48,9 @@ FinalOutfile_dir = str(sys.argv[2]) + '/out/'
 
 print('\nBuilding list of blast IDs \n')
 
-out = CaliWDout + 'blast_id_list.csv'
-final_df = pd.DataFrame()
+blast_out = CaliWDout + 'blast_id_list.csv'
+out_file = CaliWDout + 'CaliOut.csv'
+
 
 def blast_id_list_builder():
 	blast_id_list = []
@@ -60,19 +60,30 @@ def blast_id_list_builder():
 			if row[0] not in blast_id_list:
 				blast_id_list.append(row[0])
 	blast_id_list = list(dict.fromkeys(blast_id_list))
-	final_df['Blast_ID'] = blast_id_list
-	final_df.to_csv(out, index=False)
+	blast_id_df = pd.DataFrame(blast_id_list)
+	blast_id_df.to_csv(blast_out, index=False)
+	
 
+		
 
 command = 'ls ' + CaliWDout
 check = str(subprocess.check_output(command, shell=True))
 
 if 'blast_id_list.csv' not in check:
 	blast_id_list_builder()
-	blast_id_list = pd.read_csv(out, header=None, dtype = str)
+	blast_id_list = pd.read_csv(blast_out, header=None, dtype = str).values.tolist()
 else:
-	blast_id_list = pd.read_csv(out, header=None, dtype = str)
+	blast_id_list = pd.read_csv(blast_out, header=None, dtype = str).values.tolist()
 
+
+
+blast_id_list = [item for sublist in blast_id_list for item in sublist]
+dot_lst = ['.', '.', '.']
+for i in dot_lst:
+	blast_id_list.insert(1, i)
+
+blast_id_df = pd.DataFrame(blast_id_list[1:])
+#blast_id_df.to_csv(out_file, index=False)
 
 
 print('Blast ID list built \n')
@@ -133,25 +144,36 @@ def compiler(chunk):
 # Create data frame with all contigs related to blast IDs and tpm. Then write to CSV
 
 
-def ID_tpm_combiner(chunk):
-	for index, row in chunk.iterrows():
+def ID_tpm_combiner(dat):
+	
+	out_df = pd.DataFrame()
+
+	for index, row in dat.iterrows():
+	#for index, row in chunk.iterrows():
 		species = row[0]
 		SRR = row[1]
 		sex = row[2]
 		SexDeterm = row[4]
 		tpm_list = []
+		#tpm_list.append(SRR)
 		tpm_list.append(species)
 		tpm_list.append(SexDeterm)
 		tpm_list.append(sex)
-		cali_abundance_file = FinalOutfile_dir + SRR + '_CaliOut.csv'
+		cali_abundance_file = CaliWDout + SRR + '_CaliOut.csv'
 		cali_abundance_df = pd.read_csv(cali_abundance_file, header=None, dtype={0: str, 1: str, 2: float})
-		for i in blast_id_list.values.tolist()[1:]:
+		for i in blast_id_list[4:]:
 			search_out = cali_abundance_df[cali_abundance_df[1].str.match(str(i))]
 			tpm = search_out[2].sum()
 			tpm_list.append(tpm)
-		final_df[SRR] = tpm_list
+		
+		out_df[SRR] = tpm_list
+	
+	return out_df.values.tolist()
 
 
+def collect_results(result):
+    """Uses apply_async's callback to setup up a separate Queue for each process"""
+    results.extend(result)	
 	
 
 
@@ -167,14 +189,34 @@ pool.close()
 
 print('Complilation complete \n')
 
+
+
+
 print('Producing final output file \n')
 
-pool = mp.Pool(processes=n_processes)
-result = pool.map(ID_tpm_combiner, chunks)
-pool.close()  
+#pool = mp.Pool(processes=n_processes)
+#result = pool.map(ID_tpm_combiner, chunks)
+#pool.close()  
+
+################################################################################################33
+
+results = []
+
+if __name__ == "__main__":
 
 
-out = FinalOutfile_dir + 'CaliOut.csv'
-final_df.to_csv(out, index=False)
+	# Repeats the compute intensive operation on 10 data frames concurrently
+	pool = mp.Pool(processes=n_processes)
+	for i in range(dat.shape[1]): 
+		pool.apply_async(ID_tpm_combiner, args=(dat, ), callback=collect_results)
+	pool.close()
+	pool.join()
+
+	# Converts list of lists to a data frame
+	out_df = pd.DataFrame(results)
+	
+	final_df = pd.concat([blast_id_df, out_df], axis = 1, sort = False)
+
+	final_df.to_csv(out_file, index=False)
 
 print('Program complete \n')

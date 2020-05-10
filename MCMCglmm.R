bfa6ee73b2ff359401227e906cf5ab87627e016d
jrsykes <jamie_r_sykes@outlook.com>
2020-05-10 17:33:24 +0100

    .

--- MCMCglmm.R
+++ MCMCglmm.R
@@ -2,92 +2,122 @@ library(ape)
 library(MCMCglmm)
 library(ggbiplot)
 library(dplyr)
-#library(rlist)
+library(data.table)
 library(tidyverse)
 
+########### Load data
+# Load phylogentic tree
 phylo<-read.nexus("/home/jamie/Documents/2020_gene_expression_study/dat/cladogram.nex")
 
-#dat <- read.csv(file = "/home/jamie/Documents/2020_gene_expression_study/cali_test/CaliWD/out/cali_out.csv", header = FALSE)
+# Load data from cali.py program
 dat <- read.csv(file = "/home/jamie/Documents/2020_gene_expression_study/dat/cali_out.csv", header = FALSE, 
                 stringsAsFactors=FALSE)
 
-##### Tidy data set and split gene data from non-gene data
+############# Tidy data set and split gene data from non-gene data
 dat = setNames(data.frame(t(dat[,-1])), dat[,1])
 rownames(dat) <- NULL
 
 non_gene_dat <- subset(dat, select = c(SRR, species, SexDeterm, sex))
 gene_dat <- subset(dat, select = -c(SRR, species, SexDeterm, sex))
 
-####### Transform all values from factor to numeric
+# Transform gene tmp values from factor to numeric
 gene_dat[] <- lapply(gene_dat, function(x) {
     if(is.factor(x)) as.numeric(as.character(x)) else x
 })
-#sapply(gene_dat, class  )
-
-
-#### Merege gene and non-gene data
-cleaned_dat <- bind_cols(non_gene_dat, gene_dat)
-
-### Set row names as SRR
-rownames(cleaned_dat) <- cleaned_dat[,1]
-cleaned_dat[,1] <- NULL
-
-#rownames(non_gene_dat) <- non_gene_dat[,1]
-#non_gene_dat[,1] <- NULL
-
-clean_gene_dat <- subset(cleaned_dat, select = -c(species, SexDeterm, sex))
+sapply(gene_dat, class  )
 
 # Remove genes with var = 0
-clean_gene_dat <- clean_gene_dat[,apply(clean_gene_dat, 2, var, na.rm=TRUE) != 0]
+gene_dat <- gene_dat[,apply(gene_dat, 2, var, na.rm=TRUE) != 0]
 
+################ Reduce dimentionality of gene transcriptiion data via principal component analysis
+# Run PCA
+prin_comp <- prcomp(t(gene_dat), scale. = T)
 
-# PCA
-prin_comp <- prcomp(t(clean_gene_dat), scale. = T)
-
-principal.components <- prin_comp$rotation
+# Asses first n principal components
+principal.components <- prin_comp$rotation[,1:10]
 prin_comp$rotation[,1:10]
 
-ggbiplot(prin_comp, scale = 0, pc.biplot = TRUE, labels = row.names(t(clean_gene_dat)), varname.size = 0)
-
+# Standard deviation and variance of PCs
 std_dev <- prin_comp$sdev
 pr_var <- std_dev^2
 
 #Total var explained/PC
-pr_var[1:14]
+pr_var[1:10]
 
 # % var explained by first n PCs
 prop_varex <- pr_var/sum(pr_var)
-prop_varex[1:14]
-sum(prop_varex[1:14])
+prop_varex[1:10]
+sum(prop_varex[1:7])
+
+# Plot first two principal comonenets
+ggbiplot(prin_comp, scale = 0, pc.biplot = TRUE, labels = row.names(t(clean_gene_dat)), varname.size = 0)
 
 # Scree plot
 plot(prop_varex, xlab = "Principal Component",
      ylab = "Proportion of Variance Explained",
      type = "b")
 
-SRR = non_gene_dat[,1]
-principal.components <- cbind(principal.components, SRR)
-colnames(principal.components) <- c(1:ncol(principal.components))
+######## Preping final data frame for MCMCglmm
 
-1:ncol(principal.components)
+# Convert principal component values to numeric
+pc.matrix <- data.matrix(principal.components)
+principal.components <- as.data.frame(pc.matrix)
 
-non_gene_dat[,1]
-principal.components[1:2,1:2]
-non_gene_dat[1:5,1:4]
+# Set row names of PC data to SRR numbers
+rownames(principal.components) <- non_gene_dat[,1]
+principal.components <- as.data.frame(principal.components)
 
-final.df <- merge(non_gene_dat, principal.components, by=rownames)
+# Add SRR numbers as a column to allow merging with non-genetic data frame
+principal.components <- tibble::rownames_to_column(principal.components, "VALUE")
+colnames(principal.components)[1] <- 'SRR'
 
-########## Phylo
+# Merge principal components with non-genetic data to create final data frame
+final.df <- merge(non_gene_dat, principal.components, by='SRR')
+colnames(final.df)[2] <- 'phylo'
+
+# Set sex and sex determination system columns to factor and then merge into one column. 
+# e.g. haplo male, haplo female...
+final.df$sex <- as.factor(final.df$sex)
+final.df$SexDeterm <- as.factor(final.df$SexDeterm)
+final.df$SexD.sex <- paste(final.df$SexDeterm,final.df$sex)
+
+# Create inverse matrix of phylogenetic correlation data from phylo object
 inv.phylo<-inverseA(phylo,nodes="TIPS",scale=TRUE)  
 
-###### MCMCglmm
-prior<-list(G=list(G1=list(V=1,nu=0.02)),R=list(V=1,nu=0.02))
+########### MCMCglmm
 
-model<-MCMCglmm(SexDeterm~random=~phylo*sex*,
-  family="gaussian",ginverse=list(phylo=inv.phylo$Ainv),prior=prior,
-  data=dat,nitt=5000000,burnin=1000,thin=500)
+### Full model
+prior2.2 <- list(G = list(G1 = list(V = diag(7), n = 6.002)), 
+                 R = list(V = diag(7), n = 6.002))
+
+model2.2<-MCMCglmm(cbind(PC1, PC2, PC3, PC4, PC5, PC6, PC7)~trait-1 + trait:SexD.sex,
+                                  random=~us(trait):phylo,
+                                  rcov=~us(trait):units,family=c("gaussian", "gaussian", "gaussian", "gaussian", "gaussian", "gaussian", "gaussian"),
+                                  ginverse=list(phylo=inv.phylo$Ainv),
+                                  data=final.df, 
+                                  prior = prior2.2,
+                                  nitt=5000000,burnin=1000,thin=500)
+
+# Output assesment
+
+par(mar=c(1,1,1,1))
+plot(model2.2$Sol)
+plot(model2.2$VCV)
+
+summary(model2.2)
+autocorr(model2.2$VCV)
+
+posterior.mode(model2.2$VCV)
+HPDinterval(model2.2$VCV)
+
+# Testing phylogenetic effect via deviance information criterion
+
+model2.2.no.phy <- MCMCglmm(cbind(PC1, PC2, PC3, PC4, PC5, PC6, PC7)~trait-1 + trait:SexD.sex,
+                   data=final.df, 
+                   nitt=5000000,burnin=1000,thin=500)
 
 
+summary(model2.2.no.phy)
 
 
 

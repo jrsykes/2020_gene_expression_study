4fe2cdecb401bc4e0b0c33d32bc103b98d1aee19
jrsykes <jamie_r_sykes@outlook.com>
2020-05-14 13:52:32 +0100

    .

--- hoonah.py
+++ hoonah.py
@@ -27,143 +27,139 @@ for index, row in dat.iterrows():
 
 for i in (list(dict.fromkeys(species_list))):
 	SpeciesCheck = str(subprocess.check_output('ls ' +  WD + '/raw/', shell=True))
-	if i in SpeciesCheck:
-		print (i)
-exit()#
+	if i not in SpeciesCheck:
+		species = i
 
-#	if i not in SpeciesCheck:
-#		species = i#
-#
 
-#		print(f"{bcolors.OKBLUE}################################")
-#		print(f"{bcolors.OKGREEN}Download SRR files, QC and trim")
-#		print(f"{bcolors.OKBLUE}################################")#
-#
+		print(f"{bcolors.OKBLUE}################################")
+		print(f"{bcolors.OKGREEN}Download SRR files, QC and trim")
+		print(f"{bcolors.OKBLUE}################################")
 
-#		for index, row in dat.iterrows():
-#			try:
-#				if row[0] == species:
-#					SRR = row[1]
-#					sex = row[2]
-#					layout = row[3]
-#					command = sbatch + scripts + '/download_trim_qc.sh ' + species + ' ' + SRR + ' ' + sex + ' ' + layout + ' ' + WD
-#					subprocess.Popen([command], shell=True)
-#					time.sleep(5)
-#					check = int(subprocess.check_output(squeue + '--user=' + user + ' | grep new_ | wc -l', shell=True))
-#					while check > 1:
-#						time.sleep(10)
-#						check = int(subprocess.check_output(squeue + '--user=' + user + ' | grep new_ | wc -l', shell=True))
-#			except:
-#				pass
-#		check2 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
-#		while 'download' in check2:
-#						time.sleep(10)
-#						check2 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))#
-#
 
-#		print(f"{bcolors.OKBLUE}################################")
-#		print(f"{bcolors.OKGREEN}Runing Trinity, BUSCO and Blast")
-#		print(f"{bcolors.OKBLUE}################################")#
+		for index, row in dat.iterrows():
+			try:
+				if row[0] == species:
+					SRR = row[1]
+					sex = row[2]
+					layout = row[3]
+					command = sbatch + scripts + '/download_trim_qc.sh ' + species + ' ' + SRR + ' ' + sex + ' ' + layout + ' ' + WD
+					subprocess.Popen([command], shell=True)
+					time.sleep(5)
+					check = int(subprocess.check_output(squeue + '--user=' + user + ' | grep new_ | wc -l', shell=True))
+					while check > 1:
+						time.sleep(10)
+						check = int(subprocess.check_output(squeue + '--user=' + user + ' | grep new_ | wc -l', shell=True))
+			except:
+				pass
+		check2 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
+		while 'download' in check2:
+						time.sleep(10)
+						check2 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
 
-#		df_paired = pd.DataFrame()
-#		df_single = pd.DataFrame()#
-#
 
-#		for index, row in dat.iterrows():
-#			if row[0] == species:
-#				if row[3] == 'PAIRED':
-#					df_paired = df_paired.append(row[0:4], ignore_index=True)
-#				if row[3] == 'SINGLE':
-#					df_single = df_single.append(row[0:4], ignore_index=True)#
+		print(f"{bcolors.OKBLUE}################################")
+		print(f"{bcolors.OKGREEN}Runing Trinity, BUSCO and Blast")
+		print(f"{bcolors.OKBLUE}################################")
 
-###############################################################################################################################
-## Trinity paired#
+		df_paired = pd.DataFrame()
+		df_single = pd.DataFrame()
 
-#		check3 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))
-#		while check3 > 2:
-#			time.sleep(10)
-#			check3 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))#
-#
 
-#		if df_paired.empty == False:
-#			command = sbatch + scripts + '/trinity_busco_blast.sh ' + species + ' PAIRED' + WD
-#			subprocess.Popen([command], shell=True)#
+		for index, row in dat.iterrows():
+			if row[0] == species:
+				if row[3] == 'PAIRED':
+					df_paired = df_paired.append(row[0:4], ignore_index=True)
+				if row[3] == 'SINGLE':
+					df_single = df_single.append(row[0:4], ignore_index=True)
 
-###############################################################################################################################
-## Trinity single #
+##############################################################################################################################
+# Trinity paired
 
-#		check4 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))
-#		while check4 > 2:
-#			time.sleep(10)
-#			check4 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))#
-#
+		check3 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))
+		while check3 > 2:
+			time.sleep(10)
+			check3 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))
 
-#		if df_single.empty == False:
-#			command = sbatch + scripts + '/trinity_busco_blast.sh ' + species + ' SINGLE' + WD
-#			subprocess.Popen([command], shell=True)#
 
-################################################################################################################################
-#
+		if df_paired.empty == False:
+			command = sbatch + scripts + '/trinity_busco_blast.sh ' + species + ' PAIRED' + WD
+			subprocess.Popen([command], shell=True)
 
-#		time.sleep(10)#
+##############################################################################################################################
+# Trinity single 
 
-#		check5 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
-#		while 'trinity' in check5:
-#						time.sleep(10)
-#						check5 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))#
-#
+		check4 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))
+		while check4 > 2:
+			time.sleep(10)
+			check4 = int(subprocess.check_output(squeue + '--user=' + user + ' | grep trinity | wc -l', shell=True))
 
-#		print(f"{bcolors.OKBLUE}##############################################")
-#		print(f"{bcolors.OKGREEN}Maping SRA libraries to de novo transcriptome")
-#		print(f"{bcolors.OKBLUE}##############################################")#
 
-#		if df_paired.empty == False and df_single.empty == False:
-#			DUEL_LAYOUT = 'YES'
-#		else:
-#			DUEL_LAYOUT = 'NO'#
-#
+		if df_single.empty == False:
+			command = sbatch + scripts + '/trinity_busco_blast.sh ' + species + ' SINGLE' + WD
+			subprocess.Popen([command], shell=True)
 
-#		for index, row in dat.iterrows():
-#			try:
-#				if row[0] == species:
-#					SRR = row[1]
-#					sex = row[2]
-#					layout = row[3]		#
+##############################################################################################################################
 
-#					command = sbatch + scripts + '/map.sh ' + species + ' ' + SRR + ' ' + sex + ' ' + layout + ' ' + DUEL_LAYOUT + WD
-#					subprocess.Popen([command], shell=True)
-#			except:
-#				pass#
 
-#		time.sleep(5)#
+		time.sleep(10)
 
-#		check6 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
-#		while 'map' in check6:
-#						time.sleep(10)
-#						check6 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))#
-#
+		check5 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
+		while 'trinity' in check5:
+						time.sleep(10)
+						check5 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
 
-#		print(f"{bcolors.OKBLUE}################################")
-#		print(f"{bcolors.OKGREEN}Filtering contaminant sequences")
-#		print(f"{bcolors.OKBLUE}################################")#
-#
-#
 
-#		for index, row in dat.iterrows():
-#			try:
-#				if row[0] == species:
-#					SRR = row[1]
-#					sex = row[2]
-#					layout = row[3]		#
+		print(f"{bcolors.OKBLUE}##############################################")
+		print(f"{bcolors.OKGREEN}Maping SRA libraries to de novo transcriptome")
+		print(f"{bcolors.OKBLUE}##############################################")
 
-#					command = sbatch + scripts + '/blob.sh ' + species + ' ' + SRR + ' ' + layout + WD
-#					subprocess.Popen([command], shell=True)
-#			except:
-#				pass#
-#
-#
-#
+		if df_paired.empty == False and df_single.empty == False:
+			DUEL_LAYOUT = 'YES'
+		else:
+			DUEL_LAYOUT = 'NO'
 
-#		print(f"{bcolors.OKBLUE}##################")
-#		print(f"{bcolors.OKGREEN}Pipeline complete")
-#		print(f"{bcolors.OKBLUE}##################")
+
+		for index, row in dat.iterrows():
+			try:
+				if row[0] == species:
+					SRR = row[1]
+					sex = row[2]
+					layout = row[3]		
+
+					command = sbatch + scripts + '/map.sh ' + species + ' ' + SRR + ' ' + sex + ' ' + layout + ' ' + DUEL_LAYOUT + WD
+					subprocess.Popen([command], shell=True)
+			except:
+				pass
+
+		time.sleep(5)
+
+		check6 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
+		while 'map' in check6:
+						time.sleep(10)
+						check6 = str(subprocess.check_output(squeue + '--user=' + user, shell=True))
+
+
+		print(f"{bcolors.OKBLUE}################################")
+		print(f"{bcolors.OKGREEN}Filtering contaminant sequences")
+		print(f"{bcolors.OKBLUE}################################")
+
+
+
+		for index, row in dat.iterrows():
+			try:
+				if row[0] == species:
+					SRR = row[1]
+					sex = row[2]
+					layout = row[3]		
+
+					command = sbatch + scripts + '/blob.sh ' + species + ' ' + SRR + ' ' + layout + WD
+					subprocess.Popen([command], shell=True)
+			except:
+				pass
+
+
+
+
+		print(f"{bcolors.OKBLUE}##################")
+		print(f"{bcolors.OKGREEN}Pipeline complete")
+		print(f"{bcolors.OKBLUE}##################")
